0c70a20458743e58be75ac0ca0fd1709d872feb7
Cameron Beacham <Cameron.Beacham@blackbuad.me>
2018-04-02 13:24:53 -0400

    adding physical attachment code snippets

---
+++ code-samples/physical-attachment-thumbnail/node-js/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var jimp = require('jimp');
+
+jimp.read('https://github.com/recurser/exif-orientation-examples/blob/master/Landscape_2.jpg?raw=true')
+    .then(function (image) {
+        // resize and crop image to 100px by 100px keeping the image centered
+        image.cover(100, 100)
+            // apply any exif orientation transforms
+            .exifRotate() 
+            .getBuffer(jimp.MIME_JPEG, function(err, buffer) {
+                // make PUT request to thumbnail_upload_url with buffer
+            })
+}).catch(function (err) {
+    console.error(err);
+});
+
+app.get('/', function (req, res) {
+                  res.send(buffer);
+                });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
